1bebb0500f05eea050fb2332680a7ee00f1e9024
TrustfulDev <sssstevenvu@gmail.com>
2025-02-24 20:09:18 -0800

    Visual Settings
    Added resolution, graphics quality, and fullscreen toggling

--- GonFiles/Assets/UI/Scripts/SettingsMenu.cs
+++ GonFiles/Assets/UI/Scripts/SettingsMenu.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class VolumeSettings : MonoBehaviour
 {
@@ -10,6 +12,34 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider sfxSlider;
     [SerializeField] private Slider uiSlider;
 
+    private Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     private void OnEnable()
     {
         // Sync sliders with saved values when the settings menu is opened
@@ -17,6 +47,22 @@ public class VolumeSettings : MonoBehaviour
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 1f);
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume", 1f);
         uiSlider.value = PlayerPrefs.GetFloat("uiVolume", 1f);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resIndex)
+    {
+        Resolution res = resolutions[resIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 
     public void SetMasterVolume()
@@ -32,12 +78,14 @@ public class VolumeSettings : MonoBehaviour
         gonMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("musicVolume", volume);
     }
+
     public void SetSfxVolume()
     {
         float volume = sfxSlider.value;
         gonMixer.SetFloat("Sfx", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("sfxVolume", volume);
     }
+
     public void SetUiVolume()
     {
         float volume = uiSlider.value;

0b485833a0dc54ab136cff4901fc6c98f0a04f8c
TrustfulDev <sssstevenvu@gmail.com>
2025-02-25 23:37:11 -0800

    Blocking out levels 1 & 2
    Experimenting with transitions here

--- GonFiles/Assets/UI/Scripts/MainMenu.cs
+++ GonFiles/Assets/UI/Scripts/MainMenu.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    // Individual Elements to Hide
+    [SerializeField] private GameObject logo;
+    [SerializeField] private Image bg;
+    [SerializeField] private GameObject startBtn;
+    [SerializeField] private Button settingsBtn;
+    [SerializeField] private Button creditsBtn;
+    [SerializeField] private Button quitBtn;
+
+    private int transitionDuration = 4;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +30,44 @@ public class MainMenu : MonoBehaviour
     }
 
     // Play Logic
+    public void StartGame()
+    {
+        startBtn.SetActive(false);
+        HideMenu();
 
+        Invoke(nameof(DestroySelf), transitionDuration);
+    }
 
     // Quit Logic
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void HideMenu()
+    {
+        // Vignette
+        bg.CrossFadeAlpha(0, 3, false);
+
+        // Settings Btn
+        settingsBtn.enabled = false;
+        TextMeshProUGUI settingsTxt = settingsBtn.GetComponentInChildren<TextMeshProUGUI>();
+        settingsTxt.CrossFadeAlpha(0, 1, false);
+
+        // Credits Btn
+        creditsBtn.enabled = false;
+        TextMeshProUGUI creditTxt = creditsBtn.GetComponentInChildren<TextMeshProUGUI>();
+        creditTxt.CrossFadeAlpha(0, 1.5f, false);
+
+        // Quit Btn
+        quitBtn.enabled = false;
+        TextMeshProUGUI quitTxt = quitBtn.GetComponentInChildren<TextMeshProUGUI>();
+        quitTxt.CrossFadeAlpha(0, 1.75f, false);
+    }
+
+    // Destroy after starting game
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }

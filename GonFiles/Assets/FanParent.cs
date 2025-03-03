47674d246cde7184a671056e687ba4c45b40ba79
RobynIsPoppyn <ddecastro470@gmail.com>
2025-03-03 02:59:56 -0800

    Fan with wind

--- GonFiles/Assets/FanParent.cs
+++ GonFiles/Assets/FanParent.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class FanParent : MonoBehaviour
 {
     Fan windZone; 
+
+    public float interval = 2f;
     public bool FanOn{get; private set;}
     public void Start(){
         windZone = transform.GetChild(0).GetComponent<Fan>();
 
     }
+    
+    private float m_timePass = 0f;
+    public void Update(){
+        if (interval != 0){
+            m_timePass += Time.deltaTime;
+
+            if (m_timePass > interval){
+                m_timePass = 0;
+                Toggle();
+            }
+        }
+    }
 
     public void Toggle(){
         FanOn = !FanOn;
-        windZone.enabled = FanOn;
+        windZone.gameObject.SetActive(FanOn);
     }
 }

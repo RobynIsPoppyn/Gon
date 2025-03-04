938807d42cea583dca006549ff84fef066acf900
RobynIsPoppyn <ddecastro470@gmail.com>
2025-03-03 20:01:34 -0800

    Update camera thing

--- GonFiles/Assets/Player/PlayerScripts/PlayerMovement.cs
+++ GonFiles/Assets/Player/PlayerScripts/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : ShiftZ
     
     public virtual void Start()
     {
-        cat = Camera.main.transform.parent.parent.GetComponent<CameraAnimationTriggers>();
+        cat = GameObject.Find("CameraContainer").GetComponent<CameraAnimationTriggers>();
         base.Start();
         currShape = AvailShapes[0]; 
         currShapeIndex = 0;
